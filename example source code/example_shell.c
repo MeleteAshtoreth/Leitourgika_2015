b666218e12bc589d06959188b64cc0518b8c3a0a
MeleteAshtoreth <j_4_us@hotmail.com>
2015-11-07 17:21:42 +0200

    add extra code

--- example source code/example_shell.c
+++ example source code/example_shell.c
@@ -4,6 +4,80 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*
+All of the program examples included in this chapter are part of a simple shell program. 
+This section presents data structures and utility functions which are used throughout the example.
+
+The sample shell deals mainly with two data structures. 
+The job type contains information about a job, which is a set of subprocesses linked together with pipes. 
+The process type holds information about a single subprocess. Here are the relevant data structure declarations:
+*/
+/* A process is a single process.  */
+typedef struct process
+{
+  struct process *next;       /* next process in pipeline */
+  char **argv;                /* for exec */
+  pid_t pid;                  /* process ID */
+  char completed;             /* true if process has completed */
+  char stopped;               /* true if process has stopped */
+  int status;                 /* reported status value */
+} process;
+
+/* A job is a pipeline of processes.  */
+typedef struct job
+{
+  struct job *next;           /* next active job */
+  char *command;              /* command line, used for messages */
+  process *first_process;     /* list of processes in this job */
+  pid_t pgid;                 /* process group ID */
+  char notified;              /* true if user told about stopped job */
+  struct termios tmodes;      /* saved terminal modes */
+  int stdin, stdout, stderr;  /* standard i/o channels */
+} job;
+
+/* The active jobs are linked into a list.  This is its head.   */
+job *first_job = NULL;
+
+/*
+Here are some utility functions that are used for operating on job objects.
+*/
+/* Find the active job with the indicated pgid.  */
+job *
+find_job (pid_t pgid)
+{
+  job *j;
+
+  for (j = first_job; j; j = j->next)
+    if (j->pgid == pgid)
+      return j;
+  return NULL;
+}
+
+/* Return true if all processes in the job have stopped or completed.  */
+int
+job_is_stopped (job *j)
+{
+  process *p;
+
+  for (p = j->first_process; p; p = p->next)
+    if (!p->completed && !p->stopped)
+      return 0;
+  return 1;
+}
+
+/* Return true if all processes in the job have completed.  */
+int
+job_is_completed (job *j)
+{
+  process *p;
+
+  for (p = j->first_process; p; p = p->next)
+    if (!p->completed)
+      return 0;
+  return 1;
+}
+
+
 pid_t shell_pgid;
 struct termios shell_tmodes;
 int shell_terminal;
@@ -54,6 +128,8 @@ init_shell ()
 /*
 Here is the function from the sample shell program that is responsible for launching a program. 
 The function is executed by each child process immediately after it has been forked by the shell, and never returns. 
+If the shell is not running interactively, this function does not do anything with process groups or signals. 
+Remember that a shell not performing job control must keep all of its subprocesses in the same process group as the shell itself. 
 */
 void
 launch_process (process *p, pid_t pgid,
@@ -104,4 +180,276 @@ launch_process (process *p, pid_t pgid,
   execvp (p->argv[0], p->argv);
   perror ("execvp");
   exit (1);
+}
+
+/*
+Next, here is the function that actually launches a complete job. 
+After creating the child processes, this function calls some other functions to put the newly created job into the foreground or background; 
+these are discussed in Foreground and Background. 
+*/
+
+void
+launch_job (job *j, int foreground)
+{
+  process *p;
+  pid_t pid;
+  int mypipe[2], infile, outfile;
+
+  infile = j->stdin;
+  for (p = j->first_process; p; p = p->next)
+    {
+      /* Set up pipes, if necessary.  */
+      if (p->next)
+        {
+          if (pipe (mypipe) < 0)
+            {
+              perror ("pipe");
+              exit (1);
+            }
+          outfile = mypipe[1];
+        }
+      else
+        outfile = j->stdout;
+
+      /* Fork the child processes.  */
+      pid = fork ();
+      if (pid == 0)
+        /* This is the child process.  */
+        launch_process (p, j->pgid, infile,
+                        outfile, j->stderr, foreground);
+      else if (pid < 0)
+        {
+          /* The fork failed.  */
+          perror ("fork");
+          exit (1);
+        }
+      else
+        {
+          /* This is the parent process.  */
+          p->pid = pid;
+          if (shell_is_interactive)
+            {
+              if (!j->pgid)
+                j->pgid = pid;
+              setpgid (pid, j->pgid);
+            }
+        }
+
+      /* Clean up after pipes.  */
+      if (infile != j->stdin)
+        close (infile);
+      if (outfile != j->stdout)
+        close (outfile);
+      infile = mypipe[0];
+    }
+
+  format_job_info (j, "launched");
+
+  if (!shell_is_interactive)
+    wait_for_job (j);
+  else if (foreground)
+    put_job_in_foreground (j, 0);
+  else
+    put_job_in_background (j, 0);
+}
+
+/*
+Now let’s consider what actions must be taken by the shell when it launches a job into the foreground, and how this differs from what must be done when a background job is launched.
+
+When a foreground job is launched, the shell must first give it access to the controlling terminal by calling tcsetpgrp. 
+Then, the shell should wait for processes in that process group to terminate or stop. This is discussed in more detail in Stopped and Terminated Jobs.
+
+When all of the processes in the group have either completed or stopped, the shell should regain control of the terminal for its own process group by calling tcsetpgrp again. 
+Since stop signals caused by I/O from a background process or a SUSP character typed by the user are sent to the process group, normally all the processes in the job stop together.
+
+The foreground job may have left the terminal in a strange state, so the shell should restore its own saved terminal modes before continuing. 
+In case the job is merely stopped, the shell should first save the current terminal modes so that it can restore them later if the job is continued. 
+The functions for dealing with terminal modes are tcgetattr and tcsetattr; these are described in Terminal Modes. 
+*/
+
+/* Put job j in the foreground.  If cont is nonzero,
+   restore the saved terminal modes and send the process group a
+   SIGCONT signal to wake it up before we block.  */
+
+void
+put_job_in_foreground (job *j, int cont)
+{
+  /* Put the job into the foreground.  */
+  tcsetpgrp (shell_terminal, j->pgid);
+
+  /* Send the job a continue signal, if necessary.  */
+  if (cont)
+    {
+      tcsetattr (shell_terminal, TCSADRAIN, &j->tmodes);
+      if (kill (- j->pgid, SIGCONT) < 0)
+        perror ("kill (SIGCONT)");
+    }
+
+  /* Wait for it to report.  */
+  wait_for_job (j);
+
+  /* Put the shell back in the foreground.  */
+  tcsetpgrp (shell_terminal, shell_pgid);
+
+  /* Restore the shell’s terminal modes.  */
+  tcgetattr (shell_terminal, &j->tmodes);
+  tcsetattr (shell_terminal, TCSADRAIN, &shell_tmodes);
+}
+
+/*
+If the process group is launched as a background job, the shell should remain in the foreground itself and continue to read commands from the terminal.
+
+In the sample shell, there is not much that needs to be done to put a job into the background. Here is the function it uses:
+*/
+
+/* Put a job in the background.  If the cont argument is true, send
+   the process group a SIGCONT signal to wake it up.  */
+
+void
+put_job_in_background (job *j, int cont)
+{
+  /* Send the job a continue signal, if necessary.  */
+  if (cont)
+    if (kill (-j->pgid, SIGCONT) < 0)
+      perror ("kill (SIGCONT)");
+}
+
+
+/*
+When a foreground process is launched, the shell must block until all of the processes in that job have either terminated or stopped. 
+It can do this by calling the waitpid function; see Process Completion. Use the WUNTRACED option so that status is reported for processes that stop as well as processes that terminate.
+
+The shell must also check on the status of background jobs so that it can report terminated and stopped jobs to the user; this can be done by calling waitpid with the WNOHANG option. 
+A good place to put a such a check for terminated and stopped jobs is just before prompting for a new command.
+
+The shell can also receive asynchronous notification that there is status information available for a child process by establishing a handler for SIGCHLD signals. See Signal Handling.
+
+In the sample shell program, the SIGCHLD signal is normally ignored. 
+This is to avoid reentrancy problems involving the global data structures the shell manipulates. 
+But at specific times when the shell is not using these data structures—such as when it is waiting for input on the terminal—it makes sense to enable a handler for SIGCHLD. 
+The same function that is used to do the synchronous status checks (do_job_notification, in this case) can also be called from within this handler.
+
+Here are the parts of the sample shell program that deal with checking the status of jobs and reporting the information to the user.
+*/
+
+/* Store the status of the process pid that was returned by waitpid.
+   Return 0 if all went well, nonzero otherwise.  */
+
+int
+mark_process_status (pid_t pid, int status)
+{
+  job *j;
+  process *p;
+
+  if (pid > 0)
+    {
+      /* Update the record for the process.  */
+      for (j = first_job; j; j = j->next)
+        for (p = j->first_process; p; p = p->next)
+          if (p->pid == pid)
+            {
+              p->status = status;
+              if (WIFSTOPPED (status))
+                p->stopped = 1;
+              else
+                {
+                  p->completed = 1;
+                  if (WIFSIGNALED (status))
+                    fprintf (stderr, "%d: Terminated by signal %d.\n",
+                             (int) pid, WTERMSIG (p->status));
+                }
+              return 0;
+             }
+      fprintf (stderr, "No child process %d.\n", pid);
+      return -1;
+    }
+
+  else if (pid == 0 || errno == ECHILD)
+    /* No processes ready to report.  */
+    return -1;
+  else {
+    /* Other weird errors.  */
+    perror ("waitpid");
+    return -1;
+  }
+}
+
+/* Check for processes that have status information available,
+   without blocking.  */
+
+void
+update_status (void)
+{
+  int status;
+  pid_t pid;
+
+  do
+    pid = waitpid (WAIT_ANY, &status, WUNTRACED|WNOHANG);
+  while (!mark_process_status (pid, status));
+}
+
+/* Check for processes that have status information available,
+   blocking until all processes in the given job have reported.  */
+
+void
+wait_for_job (job *j)
+{
+  int status;
+  pid_t pid;
+
+  do
+    pid = waitpid (WAIT_ANY, &status, WUNTRACED);
+  while (!mark_process_status (pid, status)
+         && !job_is_stopped (j)
+         && !job_is_completed (j));
+}
+
+/* Format information about job status for the user to look at.  */
+
+void
+format_job_info (job *j, const char *status)
+{
+  fprintf (stderr, "%ld (%s): %s\n", (long)j->pgid, status, j->command);
+}
+
+/* Notify the user about stopped or terminated jobs.
+   Delete terminated jobs from the active job list.  */
+
+void
+do_job_notification (void)
+{
+  job *j, *jlast, *jnext;
+  process *p;
+
+  /* Update status information for child processes.  */
+  update_status ();
+
+  jlast = NULL;
+  for (j = first_job; j; j = jnext)
+    {
+      jnext = j->next;
+
+      /* If all processes have completed, tell the user the job has
+         completed and delete it from the list of active jobs.  */
+      if (job_is_completed (j)) {
+        format_job_info (j, "completed");
+        if (jlast)
+          jlast->next = jnext;
+        else
+          first_job = jnext;
+        free_job (j);
+      }
+
+      /* Notify the user about stopped jobs,
+         marking them so that we won’t do this more than once.  */
+      else if (job_is_stopped (j) && !j->notified) {
+        format_job_info (j, "stopped");
+        j->notified = 1;
+        jlast = j;
+      }
+
+      /* Don’t say anything about jobs that are still running.  */
+      else
+        jlast = j;
+    }
 }
